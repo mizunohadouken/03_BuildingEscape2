b8ab485281a94b577431292594856a444d38795c
DESKTOP-RQ7V5KF\Andrew <andrewmvalenz@gmail.com>
2018-04-19 20:04:52 -0700

    S3L68 Added reporting component to chairs

---
+++ BuildingEscapeTwo/Source/BuildingEscapeTwo/PositionReport.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	FString object_name = GetOwner()->GetName();
+	FString object_location = GetOwner()->GetActorLocation().ToString();
+	FString object_location2 = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s reporting at posdition %s, how about %s"), *object_name, *object_location, *object_location2);
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
